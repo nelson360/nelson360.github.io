e04efd05006551267d5961d0fb93a567dadd6eda
nelson360 <nelsonomondi99@gmail.com>
2022-11-05 04:20:49 +0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+!/usr/bin/env sh
 
 set -e
 
@@ -7,3 +7,8 @@ npm run build
 cd dist
 
 git init
+git add -A
+git commit -m 'deploy production'
+git push -f git@github.com:nelson360/nelson360.github.io.git main
+
+cd -
